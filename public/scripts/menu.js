72f126fdacd027c6c38b4dae6ba9a15f6a959540
Sammi Wong <sammiwong101302@gmail.com>
2023-11-29 23:28:21 -0500

    Added updating total cost

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -3,98 +3,98 @@ const products = [
         name: "Signature Bubble Tea",
         image: "../images/signature-bubble-tea.png",
         category: "Milk Tea",
-        price: 7.99,
+        price: 7.00,
         info: "Our signature GoBoba milk tea with sweet chewy tapioca."
     },
     {
         name: "Classic Milk Tea",
         image: "../images/classic-milk-tea.png",
         category: "Milk Tea",
-        price: 7.99,
+        price: 5.50,
         info: "Made with black tea, brown sugar and non dairy cream."
     },
     {
         name: "Lava Pearl Milk Tea",
         image: "../images/lava-pearl-milk-tea.png",
         category: "Milk Tea",
-        price: 7.99,
+        price: 8.99,
         info: "Black sugar coated tapioca with milk tea."
     },
     {
         name: "Lava Pearl Jasmine Milk Tea",
         image: "../images/lava-pearl-jasmine-milk-tea.png",
         category: "Milk Tea",
-        price: 7.99,
+        price: 7.50,
         info: "Black sugar coated tapioca with Jasmine milk tea."
     },
     {
         name: "Mango Green Tea",
         image: "../images/mango-green-tea.png",
         category: "Fruit Tea",
-        price: 8.99,
+        price: 7.50,
         info: "Tropical fresh mango, mango juice with green tea."
     },
     {
         name: "Passion Fruit Black Tea",
         image: "../images/passion-fruit-black-tea.png",
         category: "Fruit Tea",
-        price: 8.99,
+        price: 7.50,
         info: "Made with passion fruit juice and black tea."
     },
     {
         name: "Lemon Green Tea",
         image: "../images/lemon-green-tea.png",
         category: "Fruit Tea",
-        price: 1.99,
+        price: 8.00,
         info: "Lemon juice infused green tea."
     },
     {
         name: "Honey Aloe Vera",
         image: "../images/honey-aloe-vera.png",
         category: "Fruit Tea",
-        price: 1.99,
+        price: 6.00,
         info: "Black tea with aloe vera and honey."
     },
     {
         name: "Lemon Yakult",
         image: "../images/lemon-yakult.png",
         category: "Yakult",
-        price: 6.99,
+        price: 8.50,
         info: "Lemon juice with sweetened probiotic Yakult. Contains dairy. Caffeine-free."
     },
     {
         name: "Mango Yakult",
         image: "../images/mango-yakult.png",
         category: "Yakult",
-        price: 6.99,
+        price: 7.50,
         info: "Tropical fruity blend with sweetened probiotic Yakult. Caffeine-free."
     },
     {
         name: "Grapefruit Yakult",
         image: "../images/grapefruit-yakult.png",
         category: "Yakult",
-        price: 6.99,
+        price: 6.00,
         info: "Grapefruit juice with with probiotic Yakult.  Contains dairy. Caffeine-free."
     },
     {
         name: "Green Tea Yakult",
         image: "../images/green-tea-yakult.png",
         category: "Yakult",
-        price: 6.99,
+        price: 6.00,
         info: "Green tea with sweetened probiotic Yakult. Contains dairy."
     },
     {
         name: "Matcha Smoothie",
         image: "../images/matcha-smoothie.png",
         category: "Smoothie",
-        price: 6.99,
+        price: 7.50,
         info: "Japanese matcha green tea ice blended with a salty cream foam. Caffeine-free."
     },
     {
         name: "Mango Smoothie",
         image: "../images/mango-smoothie.png",
         category: "Smoothie",
-        price: 6.99,
+        price: 8.99,
         info: "Tropical fruity ice blended. Contains probiotic Yakult. Contains dairy. Caffeine-free."
     }
 ]
@@ -112,7 +112,7 @@ for (let i = 0; i < products.length; i++) {
     <div class="card">
     <img class="product-image" src="${product.image}">
     <div class="product-name">${product.name}</div>
-    <div class="product-price">$${product.price}</div>
+    <div class="product-price">$${product.price.toFixed(2)}</div>
     <div class="product-category">${product.category}</div>
     <div class="product-info">${product.info}</div>
     <button class="add-to-cart-btn">Add To Cart</button>
@@ -132,7 +132,6 @@ for (let i = 0; i < products.length; i++) {
         }
     }
 }
-
 
 //shopping cart array
 let shoppingCartArray = [];
@@ -168,11 +167,11 @@ function saveArray(array) {
 // Retrieve the cart items from localStorage when the page loads
 let updatedCart = JSON.parse(localStorage.getItem("myCartItems"));
 
-
 if (document.URL.includes("pages/shopping-cart.html")) {
     console.log(storedItems);
     // Creating each card in cart on right content
     const rightContent = document.getElementById("right-content");
+    const total = document.getElementById("total");
 
     for (let i = 0; i < storedItems.length; i++) {
         const myCartItem = storedItems[i];
@@ -184,7 +183,7 @@ if (document.URL.includes("pages/shopping-cart.html")) {
             </div>
             <div class="info">
                 <div class="myCartItem-name">${myCartItem.name}</div>
-                <div class="myCartItem-price">$${myCartItem.price}</div>
+                <div class="myCartItem-price">$${myCartItem.price.toFixed(2)}</div>
                 <div class="myCartItem-category">${myCartItem.category}</div>
             <div>
             <div>
@@ -196,60 +195,17 @@ if (document.URL.includes("pages/shopping-cart.html")) {
         rightContent.appendChild(newDiv);
     }
 
+    //Get total cost of cart item by adding the storedItems price together
+    let result = 0;
+    for (let i = 0; i < storedItems.length; i++) {
+        result += storedItems[i].price;
+        result.toFixed(2);
+    }
+    let roundedResult = result.toFixed(2);
+    total.textContent += `$${roundedResult}`;
 }
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//async
-// async function myDisplay(array) {
-//     let myPromise = new Promise(function (resolve, reject) {
-//         let myJSON = JSON.stringify(array);
-//         // Save cart items to localStorage
-//         localStorage.setItem("myCartItems", myJSON);
-//         resolve(); // Resolve after saving to localStorage
-//     });
 
-//     await myPromise; // Wait for the promise to be resolved
-//     // let storedItems = JSON.parse(localStorage.getItem("myCartItems"));
-//     // console.log(storedItems); // Check the retrieved items
-//     return JSON.parse(localStorage.getItem("myCartItems"));
-// }
-
-// addToCartBtns.forEach(addToCartBtn => {
-//     addToCartBtn.addEventListener("click", () => {
-//         myDisplay(shoppingCartArray);
-//     })
-// })
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// function handleAddToCartClick(element) {
-//     // console.log(element.parentNode);
-
-//     //Getting the card information
-//     var image = element.firstChild.nextSibling;
-//     var imageURL = image.src;
-//     var name = element.firstChild.nextSibling.nextSibling.nextSibling.textContent;
-//     var price = element.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.textContent;
-//     var category = element.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.textContent;
-
-//     console.log(name);
-//     console.log(imageURL);
-//     console.log(price);
-//     console.log(category);
-
-//     // //subtotal
-//     // let subtotal = price.toFixed(2) * quantity;
-
-//     let product = {
-//         name: name,
-//         price: price,
-//         image: imageURL,
-//         quantity: quantity,
-//         subtotal: subtotal
-//     }
-// }
-
+//ISSUE: cart item not saving after you update localstorage (by choosing new items)
 
